483b051af67ba47d65cadbc06723685b09fac4e5
Samuele <samueletasciotti@icloud.com>
2024-07-25 15:09:07 +0200

    feat: base layout + var characters

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+// Creare una funzione che generi una password;
+function pass_generator($num) {
+    // Definiamo una variabile che contenga i caratteri da generare;
+    $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+
+    <!-- Form Con chiamata in GET -->
+    <form action="GET">
+        
+        <!-- Input utente per generare una Password -->
+        <input type="number">
+
+        <!-- Tasto per invio del form -->
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
